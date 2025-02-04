09ddaf249e17b111a102be60ae0d5eea31d92c62
soldier <dboumertit@edenschool.fr>
2025-02-04 11:59:09 +0100

    feat(app): continue commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const pion = document.querySelectorAll(".pion");
 
 let counter = 0;
+let ALT_CROSS = "cross";
+let ALT_CIRCLE = "circle";
+
+let arrayPion = ["", "", "", "", "", "", "", "", ""];
+
+const imgTurn = document.getElementById("imgTurn");
 
 for (let i = 0; i < pion.length; i++) {
     const pionObject = pion[i];
@@ -8,13 +14,22 @@ for (let i = 0; i < pion.length; i++) {
     pionObject.addEventListener("click", () => {
         const img = pionObject.firstElementChild;
         if (counter % 2 == 0) {
-            img.src = "../assets/croix.svg";
-            img.alt = "croix";
-            pionObject.disabled
+            if (img.src && img.alt == "") {
+                img.src = "../assets/croix.svg";
+                img.alt = ALT_CROSS;
+                imgTurn.src = "../assets/miniRond.svg";
+                imgTurn.alt = ALT_CIRCLE;
+                pionObject.disabled;
+            }
         }else {
-            img.src = "../assets/rond.svg";
-            img.alt = "rond";
+            if (img.src && img.alt == "") {
+                img.src = "../assets/rond.svg";
+                img.alt = ALT_CIRCLE;
+                imgTurn.src = "../assets/miniCroix.svg";
+                imgTurn.alt = ALT_CROSS;
+                pionObject.disabled;
+            }
         }
         counter++
-    });
+    }); 
 };
