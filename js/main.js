fcc3f95f63b572d1b4ae9b410e041d406767a771
soldier <dboumertit@edenschool.fr>
2025-02-04 14:53:55 +0100

    feat(app): commit autonome

--- js/main.js
+++ js/main.js
@@ -10,16 +10,18 @@ const imgTurn = document.getElementById("imgTurn");
 
 for (let i = 0; i < pion.length; i++) {
     const pionObject = pion[i];
-    
+        
     pionObject.addEventListener("click", () => {
         const img = pionObject.firstElementChild;
+
+        if (img.alt) return;
+
         if (counter % 2 == 0) {
             if (img.src && img.alt == "") {
                 img.src = "../assets/croix.svg";
                 img.alt = ALT_CROSS;
                 imgTurn.src = "../assets/miniRond.svg";
                 imgTurn.alt = ALT_CIRCLE;
-                pionObject.disabled;
             }
         }else {
             if (img.src && img.alt == "") {
@@ -27,9 +29,12 @@ for (let i = 0; i < pion.length; i++) {
                 img.alt = ALT_CIRCLE;
                 imgTurn.src = "../assets/miniCroix.svg";
                 imgTurn.alt = ALT_CROSS;
-                pionObject.disabled;
             }
         }
+
+        arrayPion[i] = img.alt;
+        console.log(arrayPion);
+
         counter++
     }); 
 };
